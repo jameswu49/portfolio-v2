71c2658224d510fbe806f574f28e714571c51b8c
Jimmy Wu <jameswu49@gmail.com>
2025-05-26 14:17:04 -0700

    update css

--- src/scenes/skills/view.tsx
+++ src/scenes/skills/view.tsx
@@ -10,6 +10,9 @@ const SkillsView = ({ viewModel }: ViewProps) => {
   return (
     <div>
       <IconTab icon={BoxesIcon} label="Skills" />
+      <h1 className=" text-center text-primary-foreground text-5xl font-bold mb-10 xl:text-start">
+        <span className="text-white">My</span> Skills
+      </h1>
       <SkillIcons />
     </div>
   );
